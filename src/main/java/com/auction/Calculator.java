15280fc062e75702d0e765870b9d9276c4ca8f5c
25021915VuBaoNgoc <25021915@vnu.edu.vn>
2026-05-04 14:12:39 +0700

    add javadocs

--- src/main/java/com/auction/Calculator.java
+++ src/main/java/com/auction/Calculator.java
@@ -3,16 +3,33 @@ package com.auction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Hàm tính toán.
+ */
 public class Calculator {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(Calculator.class);
 
+  /**
+   * Hàm tính tổng.
+   *
+   * @param a so thu nhat
+   * @param b so thu 2
+   * @return tổng a + b
+   */
   public int add(int a, int b) {
     int result = a + b;
     LOGGER.info("In add() method: a={}, b={}, result={}", a, b, result);
     return result;
   }
 
+  /**
+   * Hàm tính thương.
+   *
+   * @param a so thu nhat
+   * @param b so thu 2
+   * @return thương
+   */
   public int divide(int a, int b) {
     if (b == 0) {
       LOGGER.error("a={}, b={}", a, b);
